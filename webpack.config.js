2e48385fdb46af24695735a93e7249f0f154b365
miaobuao <617802189@qq.com>
2021-12-10 20:26:56 +0800

    create_repo

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'matjs.js',
+        library: 'matjs',
+        libraryTarget: 'var',
+    },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
+    },
+    mode: "development"
+};
